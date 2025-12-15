66bafa38950fa9f0c27482c4acc0f4e2313187cd
hichamlahlou <hlahlou@frepple.com>
2025-12-15 12:28:56 +0100

    return datetime objects in the Singapore time zone

--- frepple/controllers/outbound.py
+++ frepple/controllers/outbound.py
@@ -27,8 +27,9 @@ import logging
 import pytz
 import xmlrpc.client
 from xml.sax.saxutils import quoteattr
-from datetime import datetime, timedelta, date
-from pytz import timezone
+from datetime import date, datetime, timedelta, time, timezone as datetime_timezone
+from zoneinfo import ZoneInfo
+
 import ssl
 
 try:
@@ -474,11 +475,21 @@ class exporter(object):
         )
 
     def formatDateTime(self, d, tmzone=None):
+        # 1. Ensure d is a datetime
         if isinstance(d, date) and not isinstance(d, datetime):
-            d = datetime.combine(d, datetime.min.time())
-        elif isinstance(d, str):
-            d = datetime.fromisoformat(d)
-        return d.astimezone(timezone(tmzone or self.timezone)).strftime(self.timeformat)
+            # A pure date: treat as midnight UTC and no conversion
+            dt_midnight = datetime.combine(d, time(0, 0))
+            return dt_midnight.strftime(self.timeformat)
+
+        # 2. Attach UTC tzinfo (now it's aware UTC)
+        d = d.replace(tzinfo=datetime_timezone.utc)
+
+        # 3. Convert to target tz
+        tz = ZoneInfo(tmzone or "Asia/Singapore")
+        d_local = d.astimezone(tz)
+
+        # 4. Format
+        return d_local.strftime(self.timeformat)
 
     def export_users(self):
         users = []
