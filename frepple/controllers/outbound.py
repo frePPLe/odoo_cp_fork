73a3ea97dcc79ff958ce8626aac47cb3fd63dd6a
hichamlahlou <hlahlou@frepple.com>
2026-03-12 18:16:00 +0100

    use mes start/end/dates

--- frepple/controllers/outbound.py
+++ frepple/controllers/outbound.py
@@ -2639,15 +2639,21 @@ class exporter(object):
                         if wo.id not in longest_ids
                         and qty > 1
                         and wo.id not in outsourced_ids
-                        else wo.duration_expected - wo.duration_unit
+                        else wo.duration_expected
                     )
-                    if wo.is_user_working and wo.time_ids:
+                    if (
+                        wo.id in longest_ids
+                        and wo.mes_start_date
+                        and not wo.mes_end_date
+                    ):
                         # The WO is currently being worked on
-                        for tm in wo.time_ids:
-                            if tm.date_start and not tm.date_end:
-                                time_left -= round(
-                                    (now - tm.date_start).total_seconds() / 60
-                                )
+                        time_left = (
+                            round(
+                                timedelta(minutes=wo.duration_expected)
+                                - (datetime.now() - wo.mes_start_date)
+                            ).total_seconds()
+                            / 60
+                        )
 
                     # 3 weeks for outsourced operations that haven't started yet.
                     if wo.id in outsourced_ids:
@@ -2795,40 +2801,32 @@ class exporter(object):
                     # In the "approved" status, frepple can still reschedule the MO in function of material and capacity
                     # In the "confirmed" status, frepple sees the MO as frozen and unchangeable
                     # We don't want to reschedule the subcontracted MOs
-                    if wo.state == "progress":
+                    if wo.mes_start_date and not wo.mes_end_date:
                         state = "confirmed"
-                    elif wo.state in ("done", "to_close", "cancel"):
+                    elif (
+                        wo.state in ("done", "to_close", "cancel")
+                        or wo.mes_start_date
+                        and wo.mes_end_date
+                    ):
                         state = "completed"
                     else:
                         state = "approved"
                     try:
-                        if workorder_dates.get(wo.id) and wo.state in (
+                        if workorder_dates.get(wo.id) and state in (
                             "progress",
                             "done",
                         ):
                             wo_date = ' end="%s"' % self.formatDateTime(
                                 workorder_dates.get(wo.id)
                             )
-                        elif wo.date_finished:
-                            wo_date = ' end="%s"' % self.formatDateTime(
-                                wo.date_finished
-                            )
+                        elif wo.mes_end_date:
+                            wo_date = ' end="%s"' % self.formatDateTime(wo.mes_end_date)
                         else:
-                            if wo.is_user_working:
-                                dt = now
+                            if wo.mes_start_date:
+                                dt = wo.mes_start_date
                             else:
-                                dt = max(
-                                    (
-                                        wo.date_start
-                                        if wo.date_start
-                                        else (
-                                            wo.date_planned_start
-                                            if wo.date_planned_start
-                                            else i.date_planned_start
-                                        )
-                                    ),
-                                    now,
-                                )
+                                dt = now
+
                             wo_date = ' start="%s"' % self.formatDateTime(dt)
                     except Exception:
                         wo_date = ""
