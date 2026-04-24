0d5f2010ecd5235f10a9e9aea62e9ee64557a725
hichamlahlou <hlahlou@frepple.com>
2026-04-24 12:49:53 +0200

    debug

--- frepple/controllers/outbound.py
+++ frepple/controllers/outbound.py
@@ -2467,6 +2467,8 @@ class exporter(object):
         workorder_dates = {}
         workorder_vendor = {}
 
+        import xml.etree.ElementTree as ET
+
         for i in self.generator.getData(
             "outsource.po.reference",
             search=[
@@ -2478,6 +2480,28 @@ class exporter(object):
             if i.quantity == i.delivered_qty:
                 continue
             for j in i.work_order_ids:
+                if j.id == 249745:
+                    yield ET.tostring(
+                        ET.Comment(
+                            f"FOUND: i.purchase_line_id={i.purchase_line_id.id}",
+                            indent=2,
+                        ),
+                        encoding="unicode",
+                    )
+                    yield ET.tostring(
+                        ET.Comment(
+                            f"FOUND: i.purchase_line_id.revised_line_date={i.purchase_line_id.revised_line_date}",
+                            indent=2,
+                        ),
+                        encoding="unicode",
+                    )
+                    yield ET.tostring(
+                        ET.Comment(
+                            f"FOUND: i.purchase_line_id<date_planned={i.purchase_line_id.date_planned}",
+                            indent=2,
+                        ),
+                        encoding="unicode",
+                    )
                 if (
                     i.purchase_id.partner_id
                     and i.purchase_id.partner_id.id in self.map_customers
@@ -2491,8 +2515,6 @@ class exporter(object):
                 )
                 if date_planned:
                     workorder_dates[j.id] = date_planned
-
-        import xml.etree.ElementTree as ET
 
         debug_comment = ET.Comment(
             f" DEBUG DICT: {json.dumps({i:self.formatDateTime(workorder_dates[i]) for i in workorder_dates}, indent=2)} "
